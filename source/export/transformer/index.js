28766755fd09a3ea3f667af512276fa0325ae4fb
Christian Auth <c.auth@syzygy.de>
2017-08-21 18:21:08 +0200

    fix(export.transformer): updates transformer setup

--- source/export/transformer/index.js
+++ source/export/transformer/index.js
@@ -5,12 +5,18 @@ module.exports =
 {
     JspConcatTransformer: require('./JspConcatTransformer.js').JspConcatTransformer,
     JspEmptyTransformer: require('./JspEmptyTransformer.js').JspEmptyTransformer,
+    JspInlineMacroCallTransformer: require('./JspInlineMacroCallTransformer.js').JspInlineMacroCallTransformer,
 
     transformerList:
     [
-        require('entoj-system').export.transformer.RemoveLoadModelTransformer,
-        require('./JspConcatTransformer.js').JspConcatTransformer,
-        require('./JspEmptyTransformer.js').JspEmptyTransformer,
-        require('./JspInlineMacroCallTransformer.js').JspInlineMacroCallTransformer
+        [
+            require('entoj-system').export.transformer.RemoveLoadModelTransformer,
+            require('./JspConcatTransformer.js').JspConcatTransformer,
+            require('./JspEmptyTransformer.js').JspEmptyTransformer,
+            require('./JspInlineMacroCallTransformer.js').JspInlineMacroCallTransformer
+        ],
+        [
+            require('entoj-system').export.transformer.RemoveYieldTransformer
+        ]
     ]
 };
