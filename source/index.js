667bb48ffd98899c56443deff460ad0975d92188
Christian Auth <c.auth@syzygy.de>
2017-12-19 15:40:18 +0100

    feat: adds module registration

--- source/index.js
+++ source/index.js
@@ -1,8 +1,22 @@
+'use strict';
+
 /**
- *
+ * Registers with default configurations
+ */
+function register(configuration, options)
+{
+    // Commands
+    configuration.commands.add(require('./command/JspExportCommand.js').JspExportCommand);
+}
+
+
+/**
+ * Exports
+ * @ignore
  */
 module.exports =
 {
+    register: register,
     command: require('./command/index.js'),
     configuration: require('./configuration/index.js'),
     export: require('./export/index.js'),
